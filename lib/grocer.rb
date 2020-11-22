e7b749c5313bd7c008887fab4d6a5f116955feee
Alex Marz <alexhmarz@gmail.com>
2020-11-22 21:49:21 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -23,7 +23,7 @@ def consolidate_cart(cart)
     while counter < cart.length
     item = find_item_by_name_in_collection(cart[counter][:item], shopping_cart)
       if item != nil
-        
+        item
       end
     
         
