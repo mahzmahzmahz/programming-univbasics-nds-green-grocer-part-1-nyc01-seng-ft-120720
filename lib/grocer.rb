af8fca07bcf2a85e35b1ec9c2a9d5f15df185a29
Alex Marz <alexhmarz@gmail.com>
2020-11-22 21:36:50 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -5,6 +5,7 @@ def find_item_by_name_in_collection(name, collection)
   counter = 0 
   while counter < collection.length
     if collection[counter][:item] == name
+      return 
   end
     counter += 1 
   end
