37d96e5a7e2cb7b8a388cf1ab8b8c2d46b853cfb
Alex Marz <alexhmarz@gmail.com>
2020-11-22 21:52:17 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -29,7 +29,7 @@ def consolidate_cart(cart)
           :item => cart[counter][:item],
           :price => cart[counter][:price]
           :clearance => cart[counter][:clearance]
-          
+          :count => 1 
         }
         
       end
